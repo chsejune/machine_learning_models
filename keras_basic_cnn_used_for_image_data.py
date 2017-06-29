106ff31396c2e86960c61beab9cb2ef3801b61ac
sjcLab <chsejune@naver.com>
2017-06-29 20:47:08 +0900

    keras_basic_cnn_used_for_image_data.py

--- keras_basic_cnn_used_for_image_data.py
+++ keras_basic_cnn_used_for_image_data.py
@@ -26,3 +26,12 @@ batch_size = 32 # 학습 batch size 설정
 num_classes = 4 # 분류 해야 할 class 개수
 epochs = 5000 # 학습 반복 회수
 
+# import data (numpy 형태로 저장되어 있는 이미지 데이터셋 불러오기)
+img_x = np.load("datasets/resized_img_180x120_X.npy")
+img_y = np.load("datasets/resized_img_180x120_Y.npy")
+# img_y 의 경우 class label인 0, 1, 2, ... 형태로 되어 있음
+# CNN을 keras 를 이용하여 학습시, one-hot encoding이 되어야 함
+# 예를 들어, 0,1,2 class label이 있을 때, 0은 [1,0,0]으로 변환, 1은 [0,1,0]으로 변환 ...
+# 이것을 자동으로 도와주는 코드가 바로 아래 코드
+img_y_cat = keras.utils.to_categorical(img_y, num_classes)
+
