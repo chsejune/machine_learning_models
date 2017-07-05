63e7d9216c62eb02c9cd10014cdd37c99526be75
sjcLab <chsejune@naver.com>
2017-07-05 16:26:30 +0900

    keras_extract_intermediate_layer_output.py

--- keras_extract_intermediate_layer_output.py
+++ keras_extract_intermediate_layer_output.py
@@ -10,13 +10,19 @@ __author__ = 'Sejune Cheon'
 import keras.backend as kb
 from keras.models import load_model
 
+# 데이터가 trainX 로 load 되어 있다고 가정
+trainX = [0,0,0,0]
+
 # 모델 불러오기
 model = load_model("filepath")
 
 # intermediate layer의 output을 뽑기 위한 함수 만들기
-get_feature_map = kb.function([model.layers[0].input, kb.learning_phase()], [model.layers[4].output])
-
+get_inter_layer_output = kb.function([model.layers[0].input, kb.learning_phase()], [model.layers[4].output])
 # "learning_phase" is needed for dropout and batchnormalizaiton use.
 # select option 0 - test mode, 1 - training mode
+
+# 사용법
+output = get_inter_layer_output([trainX, 0])[0]  # learning phase 값으로 0 또는 1 전달
+output = get_inter_layer_output([trainX, 0])[0].squeeze() # 혹시나 의미없는 차원수가 같이 출력될 경우 "squeeze" 함수를 써서 의미없는 차원수를 줄여준다.
 
 
